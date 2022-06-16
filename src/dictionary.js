f14809d16a2aede70c57f2cdf321ab1def0d62fa
Talilotu <nesikaufusi@gmail.com>
2022-06-16 16:13:52 +1000

    Added dictionary API integration

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictonary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    alert(`Searching for`);
+
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
