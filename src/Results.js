5014d44bbd0d8c9d2f6ebf01d0e35d2361fd2fbc
Talilotu <nesikaufusi@gmail.com>
2022-06-17 13:38:51 +1000

    added results component

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Results(props) {
-  return <h1>{props.results.word}</h1>;
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h1>{props.results.word}</h1>
+        <h2>{props.results.meanings[0]}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
